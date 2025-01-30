28c1aea325176609652290bf99a3639c8a90bc21
Tim-Crow <timmymark.workmail@gmail.com>
2025-01-30 13:55:58 +0300

    Added main component + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from "react";
-import ReactLogo from "./assets/react.svg?react";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import { Header, Footer, PageContent } from "./components";
 import "./App.css";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <ReactLogo className="logo" width={"6rem"}></ReactLogo>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <PageContent>
+        <h1>App content</h1>
+        <Routes>
+          <Route path="/" element={<div>Main page</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/register" element={<div>Registration</div>} />
+          <Route path="/users" element={<div>Users</div>} />
+          <Route path="/post" element={<div>New post</div>} />
+          <Route path="/post/:postId" element={<div>Post</div>} />
+          <Route path="*" element={<div>Error</div>} />
+        </Routes>
+      </PageContent>
+      <Footer />
     </>
   );
 };
