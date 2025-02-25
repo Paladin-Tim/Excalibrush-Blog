57ca9560dcc2a751580f0c81acc2a9749b038633
Tim-Crow <timmymark.workmail@gmail.com>
2025-02-25 13:49:04 +0300

    Pagination on main page

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,14 +1,23 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { server } from "../../bff";
+import { PAGINATION_LIMIT } from "../../bff/constants";
 import { getPosts } from "../../redux/actions";
 import { selectPostsList } from "../../redux/selectors";
-import { Carousel, Col, Row } from "antd";
+import { Carousel, Col, Row, Pagination } from "antd";
 import { CalendarOutlined, MessageOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
 
 export const Main = () => {
   const postsList = useSelector(selectPostsList);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPosts, setCurrentPosts] = useState([]);
+  const totalPages = Object.values(postsList).length;
+
+  const handleSetPage = (page) => {
+    setCurrentPage(page);
+  };
 
   const dispatch = useDispatch();
 
@@ -16,7 +25,14 @@ export const Main = () => {
     server.fetchPosts().then(({ res }) => {
       dispatch(getPosts(res));
     });
-  }, [dispatch]);
+
+    setCurrentPosts(
+      Object.values(postsList).slice(
+        PAGINATION_LIMIT * (currentPage - 1),
+        PAGINATION_LIMIT * (currentPage - 1) + PAGINATION_LIMIT,
+      ),
+    );
+  }, [dispatch, currentPage, postsList]);
 
   return (
     <article className="content__block main-page">
@@ -24,7 +40,7 @@ export const Main = () => {
         gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
         className="main-page__post-grid"
       >
-        {Object.values(postsList).map(
+        {currentPosts.map(
           ({ id, title, published_at, image_urls, comments }) => (
             <Col key={id} className="post-wrapper" sm={12} md={10} lg={8}>
               <Carousel arrows infinite autoplay className="post-gallery">
@@ -49,6 +65,14 @@ export const Main = () => {
           ),
         )}
       </Row>
+
+      <Pagination
+        align="center"
+        current={currentPage}
+        onChange={handleSetPage}
+        pageSize={3}
+        total={totalPages}
+      />
     </article>
   );
 };
